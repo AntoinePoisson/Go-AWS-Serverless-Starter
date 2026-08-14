b9aa133d1ce9d92b3e4c73c2e13c5870e183a648
Antoine Poisson <antoine.poisson@epitech.eu>
2026-08-14 10:03:36 +0200

    feat(local): DynamoDB Local and both functions

---
+++ tools/localdb/main.go
@@ -0,0 +1,53 @@
+// Command localdb creates the items table in a local DynamoDB instance.
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
+	"github.com/antoinepoisson/bootstrap-go-aws/internal/awsx"
+	"github.com/antoinepoisson/bootstrap-go-aws/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if cfg.DynamoDBEndpoint == "" {
+		log.Fatal("DYNAMODB_ENDPOINT must point to a local DynamoDB instance")
+	}
+
+	client, err := awsx.NewDynamoDBClient(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = client.CreateTable(context.Background(), &dynamodb.CreateTableInput{
+		TableName:   aws.String(cfg.ItemsTable),
+		BillingMode: types.BillingModePayPerRequest,
+		AttributeDefinitions: []types.AttributeDefinition{
+			{AttributeName: aws.String("id"), AttributeType: types.ScalarAttributeTypeS},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{AttributeName: aws.String("id"), KeyType: types.KeyTypeHash},
+		},
+	})
+
+	var inUse *types.ResourceInUseException
+	switch {
+	case errors.As(err, &inUse):
+		log.Printf("table %s already exists", cfg.ItemsTable)
+	case err != nil:
+		log.Printf("create table %s: %v", cfg.ItemsTable, err)
+		os.Exit(1)
+	default:
+		log.Printf("table %s created", cfg.ItemsTable)
+	}
+}
