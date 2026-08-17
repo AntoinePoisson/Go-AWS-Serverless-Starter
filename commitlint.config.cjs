8b9d9c6eaded85f2cfcc08e1eafdda1f4f5d3ab6
Antoine Poisson <antoine.poisson@epitech.eu>
2026-08-17 16:40:08 +0200

    ci: pin commit types to changelog sections

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,25 @@
+// The type list below and the changelog-sections of the Release Please config
+// are the same list: letting them drift means a commit that lints locally and
+// then vanishes from the changelog.
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build', // dependencies, the Lambda packaging, the toolchain
+        'chore', // hidden from the changelog
+        'ci', // workflows, hooks, linters
+        'docs', // README and the OpenAPI annotations
+        'feat', // minor bump
+        'fix', // patch bump
+        'perf',
+        'refactor',
+        'revert',
+        'style', // hidden from the changelog
+        'test',
+      ],
+    ],
+  },
 };
